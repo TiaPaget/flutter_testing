a01187a507577591f8dac4b59ce9cef164f5b247
Tia <tia.paget@outlook.com>
2024-08-16 14:30:35 +0100

    fixed boxes and display issue. Chnaged FractionallySized boxes to Container

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -20,48 +21,48 @@ class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          //top text
-          const FractionallySizedBox(
-            widthFactor: 1.0,
-            heightFactor: 0.1,
-            child: Center(
-              child: Text("Tap the part of the screen that is closest to you!",
-              style: TextStyle(fontSize: 32),
-              ),
-            ),
-          ),
-          //Mid-section touch area, with button
-          FractionallySizedBox(
-            widthFactor: 1.0,
-            heightFactor: 0.8,
-            child: Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  //Decide button
-                },
-                child: const Text("DECIDE"),
-              ),
-            ),
-          ),
-          //bottom upside down text section
-          FractionallySizedBox(
-            widthFactor: 1.0,
-            heightFactor: 0.1,
-            child: Center(
-              child: Transform.rotate(
-                angle: 3.141592, //180 degrees in radians (pi)
-                child: const Text("Tap the part of the screen that is closest to you!",
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        child: Column(
+          children: [
+            //top text
+            Container(
+              child: Center(
+                child: Text(
+                  "Tap the part of the screen that is closest to you!",
                   style: TextStyle(fontSize: 32),
                 ),
               ),
             ),
-          ),
-        ],
+            //Mid-section touch area, with button
+            Expanded(
+              child: Container(
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //Decide button
+                    },
+                    child: const Text("DECIDE"),
+                  ),
+                ),
+              ),
+            ),
+            //bottom upside down text section
+            Container(
+              child: Center(
+                child: Transform.rotate(
+                  angle: 3.141592, //180 degrees in radians (pi)
+                  child: const Text(
+                    "Tap the part of the screen that is closest to you!",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
