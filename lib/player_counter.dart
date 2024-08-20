01116b6ac7e8bf5cbf9b1b95656fd17b8f2a1fdd
Tia <tia.paget@outlook.com>
2024-08-20 14:53:34 +0100

    New class to handle users tap input. Using GestureDetector().

---
+++ lib/player_counter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PlayerCounter extends StatefulWidget {
+  const PlayerCounter({super.key});
+
+  @override
+  State<PlayerCounter> createState() => _PlayerCounterState();
+}
+
+class _PlayerCounterState extends State<PlayerCounter> {
+  int _playerCount = 0;
+  Offset? _tapPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      
+    )
+  }
+}
